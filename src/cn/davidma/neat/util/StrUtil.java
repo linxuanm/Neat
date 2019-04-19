8273a2d69892111ad49e0720de0e397d1062bd3a
David Ma <davidmaamoaix@gmail.com>
2019-04-19 09:35:50 +0800

    Added scene managing

---
+++ src/cn/davidma/neat/util/StrUtil.java
@@ -0,0 +1,13 @@
+package cn.davidma.neat.util;
+
+/**
+ * Utils for string operation.
+ * 
+ * @author David Ma
+ */
+public class StrUtil {
+	
+	public static boolean isEmpty(String str) {
+		return str == null || str.isEmpty();
+	}
+}
